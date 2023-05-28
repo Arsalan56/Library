c1da23b2e8bc5e852c3b1bcd50e4d5d4f2ec83ba
Arsalan <118858748+Arsalan56@users.noreply.github.com>
2023-05-28 10:42:31 -0400

    Make status button interactive
    The html and javascript are linked together. The status button is made
    interactive, making it change between checked and unchecked upon
    clicking the button.

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-const myLibrary = [];
+// const myLibrary = [];
 
-function Book() {}
+// function Book() {}
 
-function AddBook() {}
+// function AddBook() {}
 
-function displayBooks() {}
-s;
+// function displayBooks() {}
+
+const stat = document.querySelector('.status');
+const statImg = document.querySelector('.status > img');
+stat.addEventListener('click', () => {
+    const currentImg = statImg.getAttribute('src');
+    if (currentImg === 'svg-icons/uncheck.svg') {
+        statImg.setAttribute('src', 'svg-icons/check.svg');
+    } else {
+        statImg.setAttribute('src', 'svg-icons/uncheck.svg');
+    }
+});
